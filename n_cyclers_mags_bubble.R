848138ea30d49ff5baba59c52995220331b288b0
patriciatran <contact.patriciatran@gmail.com>
2018-02-28 11:34:51 -0600

    first commit
    First commit

--- n_cyclers_mags_bubble.R
+++ n_cyclers_mags_bubble.R
@@ -16,7 +16,7 @@ n_plot<- my_plot + geom_point() +
   ylab("Metagenome name")+
   #facet_grid(~Season, drop=TRUE) +
   ggtitle("Reads mapped to each Metagenome accross time")+
-  scale_x_date(date_breaks = "4 month", date_labels =  "%b %Y") +
+  scale_x_date(date_breaks = "3 month", date_labels =  "%b %Y") +
   theme(legend.position="bottom", panel.background = element_rect(fill='white'),
         panel.grid.major=element_line(linetype="dashed", colour="grey"),
         panel.border = element_rect(linetype = "solid", fill = NA, colour="grey"),
@@ -29,4 +29,17 @@ row.names(n_mags.matrix) <- n_mags[,2]
 n_mags.matrix <- n_mags.matrix[,-1]
 mode(n_mags.matrix) <- "numeric" 
 
-heatmap_dendogram <- heatmap(n_mags.matrix, cexCol=0.30)
+heatmap_dendogram <- heatmap(n_mags.matrix, cexCol=0.80)
+
+install.packages('ape')
+library(ape)
+# plot basic tree
+
+hc <- hclust(dist(n_mags.matrix))
+plot(hc)
+plot(as.phylo(n_mags.matrix), cex = 0.9, label.offset = 1)
+
+
+nba_heatmap <- heatmap(n_mags.matrix, Rowv=T, Colv=T,
+                       col = heat.colors(256), scale="row", 
+                       margins=c(5,10))
